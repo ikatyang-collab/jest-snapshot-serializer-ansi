5583d46bcee5438c39959cdea8819448e143ebc0
fisker <lionkay@gmail.com>
2025-03-09 12:08:24 +0800

    feat: support strip ansi in error message

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import hasAnsi from 'has-ansi'
 import stripAnsi from 'strip-ansi'
 
-export function test(value: any): boolean {
-  return typeof value === 'string' && hasAnsi(value)
+export function test(value: unknown): boolean {
+  const text = value instanceof Error ? value.message : value
+  return typeof text === 'string' && hasAnsi(text)
 }
 
-export function print(value: unknown, serialize: (value: unknown) => string) {
-  return serialize(stripAnsi(value as string))
+export function print(value: unknown) {
+  if (value instanceof Error) {
+    return `[${value.name}: ${stripAnsi(value.message)}]`
+  }
+
+  return stripAnsi(value as string)
 }
 
 export default { test, print }
